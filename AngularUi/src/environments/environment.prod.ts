5a6a93739426c984c83a227748bc132bb59d5c5c
Joe Murphy <55860945+Murphy-Joe@users.noreply.github.com>
2022-03-23 16:18:03 -0400

    Initial commit

---
+++ AngularUi/src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  sitesApi: 'http://api.camping-galaxy.com/campsites/',
+  bffApi: 'http://api.camping-galaxy.com/bff/',
+  pricingApi: 'http://api.camping-galaxy.com/accounting/',
+  defaultPricing: {
+    baseAmount: 58,
+    waterDaily: 10,
+    electricalDaily: 35,
+    lakeFrontDaily: 40,
+  },
+};
